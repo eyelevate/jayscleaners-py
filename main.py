f1b231a2a3c437b9b89079aaf36c79e1b9d93632
wondollaballa <onedough83@gmail.com>
2016-05-20 23:33:13 -0700

    os update

--- main.py
+++ main.py
@@ -7,13 +7,14 @@ import os
 
 if platform.system() == 'Darwin':  # Mac
     sys.path.append('/Library/Frameworks/Python.framework/Versions/3.5/lib/python3.5/site-packages')
-    os.environ['KIVY_METRICS_DENSITY'] = '0.8'
+    os.environ['KIVY_METRICS_DENSITY'] = '2'
     os.environ['KIVY_DPI'] = '320'
-    os.environ['KIVY_METRICS_FONTSCALE'] = '1.2'
+    os.environ['KIVY_METRICS_FONTSCALE'] = '.8'
 elif platform.system() == 'Linux':  # Linux
     sys.path.append('/')  # TODO
 elif platform.system() == 'Windows':  # Windows
     sys.path.append('/')  # TODO
+else:
     os.environ['KIVY_METRICS_DENSITY'] = '2'
     os.environ['KIVY_DPI'] = '320'
     os.environ['KIVY_METRICS_FONTSCALE'] = '0.8'
