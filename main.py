ba0bf76cd015704af0e509b3e4795ee4caed1378
wondollaballa <onedough83@gmail.com>
2017-04-06 14:12:34 -0700

    update

--- main.py
+++ main.py
@@ -1474,7 +1474,7 @@ GridLayout:
         text_size:self.size
         valign:'bottom'
         halign:'center'
-        on_press: root.parent.parent.parent.parent.parent.parent.set_item({item_id})
+        on_press_down: root.parent.parent.parent.parent.parent.parent.set_item({item_id})
         background_rgba:(.7,.3,.5,1)
         Image:
             id: item_image
